43bce838791b221038676bedd2568cecabdd829e
ritatiha <113474300+ritatiha@users.noreply.github.com>
2022-11-22 14:55:38 -0500

    Update ProductTest.java

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -20,8 +20,7 @@ public class ProductTest {
     public void checkProductPrice() {
         // I am making this test to fail
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        public double intcheck = 180;
-        assertEquals("Check the price of the product", intcheck, aProduct.getPrice());
+        assertEquals("Check the price of the product", 180, aProduct.getPrice());
     }
 
 
